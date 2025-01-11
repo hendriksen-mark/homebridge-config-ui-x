a09164ee30e73f659d00d3d9b665e0e0901f2796
Ben <43026681+bwp91@users.noreply.github.com>
2025-01-11 17:26:07 +0000

    migrate bootstrap from `v4` to `v5`

--- ui/src/app/modules/settings/settings.component.ts
+++ ui/src/app/modules/settings/settings.component.ts
@@ -99,7 +99,7 @@ export class SettingsComponent implements OnInit {
     cache: true,
   }
 
-  public readonly file = '<span class="text-monospace">ui-wallpaper.jpg</span>'
+  public readonly file = '<span class="font-monospace">ui-wallpaper.jpg</span>'
   public readonly linkDebug = '<a href="https://github.com/homebridge/homebridge-config-ui-x/wiki/Debug-Common-Values" target="_blank"><i class="fa fa-fw fa-external-link-alt"></i></a>'
 
   constructor() {}
